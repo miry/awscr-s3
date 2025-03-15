748315ffb29fdb398a85edc83cbc8a0139161f3e
Michael Nikitochkin <michael.nikitochkin@gmx.net>
2025-03-15 11:20:30 +0100

    Setup the integration tests
    Update integration tests to cover the primitive actions

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,7 +3,29 @@ require "timecop"
 require "webmock"
 require "./fixtures"
 
+# Allow to print the Crystal class methods
+# Usage:  `puts Spec::CLI.methods.sort`
+class Object
+  macro methods
+    {{ @type.methods.map &.name.stringify }}
+  end
+end
+
+class Spec::CLI
+  def tags
+    @tags
+  end
+end
+
 Spec.around_each do |example|
+  # Strategy to skip tests by tags
+  # tags = Spec.cli.tags
+  # if tags.nil? || tags.empty?
+  #   # By default, skip tests with tags and run only Unit tests
+  #   next if !example.example.all_tags.empty?
+  # end
+
+  # Timecop.return
   WebMock.reset
   # Integration tests should allow to send requests
   integration = example.example.all_tags.includes?("integration") || \
